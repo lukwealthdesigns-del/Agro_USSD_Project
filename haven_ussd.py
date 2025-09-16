370074e9e8e722ae9a376ccee5a9c3e86d854bcb
Oyedoyin Samuel <sammmie225@gmail.com>
2025-09-16 13:26:10 +0100

    new_update

--- haven_ussd.py
+++ haven_ussd.py
@@ -1,12 +1,13 @@
-
-from pathlib import Path
 import csv
+from pathlib import Path
+
 # --- Database Setup ---
 database = Path("database")
-if database.exists() is False:   
+if database.exists() is False:   # instead of if not database.exists()
     database.mkdir()
 farmers_csv = database / "farmers.csv"
 buyers_csv = database / "buyers.csv"
+
 # Initialize CSV with headers if files do not exist
 if farmers_csv.exists() is False:
     with open(farmers_csv, "w", newline="", encoding="utf-8") as f:
@@ -21,29 +22,106 @@ while True:
     option = input("Kindly Dial *620#: ")
     if option == "*620#":
         print("Welcome to Haven Agro Bank")
-        menu = int(input("Are you a farmer or a buyer?\n1. Farmer\n2. Buyer\nChoose Option: "))
-        # ---------------- Farmer Menu ----------------
+        menu = int(
+            input("Are you a farmer or a buyer?\n1. Farmer\n2. Buyer\nChoose Option:  "))
         if menu == 1:
-            menu_o = int(input("1. Register as a farmer  2. Login as a farmer\nChoose Option: "))
-            # Farmer Registration
+            menu_o = int(
+                input("1. Register as a farmer  2. Login as a farmer\nChoose Option: "))
+
             if menu_o == 1:
-                first_name = input("Enter Your First Name: ").strip().title()
-                surname = input("Enter Your Surname: ").strip().title()
-                full_name = f"{surname} {first_name}"
-                phone = input("Enter Your Phone number (11 digits): ")
-                state = input("Enter your location (state): ").strip().title()
-                city = input("Enter your location (city): ").strip().title()
-                farm_size = input("Kindly enter your farm size in acres: ")
-                crops = input("Enter your primary crops: ").strip().title()
-                pin = input("Enter Your 4 digit pin: ")
-                re_pin = input("Confirm your pin: ")
-                if pin == re_pin:
+                while True:
+                    first_name = input("Enter Your First Name: ")
+                    if first_name != "":
+                        break
+                    else:
+                        print(
+                            "Dear user, Your first name cannot be empty. Kindly input your first name and try again")
+                while True:
+                    surname = input("Enter Your Suranme: ")
+                    if surname != "":
+                        break
+                    else:
+                        print(
+                            "Dear user, Your surname cannot be empty. Kindly input your surname and try again")
+                    full_name = (surname, first_name)
+                
+                while True:
+                    phonenumber = (input("Enter Your Phone number: "))
+                    if len(phonenumber) == 11:
+                        break
+                    else:
+                        print("R-enter phone number")
+                        
+                while True:
+                    location1 = input(
+                        "Enter your location (state): ").strip().title()
+                    if location1 != "":
+                        break
+                    else:
+                        print(
+                            "Dear user, Your state cannot be empty. Kindly input your state and try again")
+                    
+                while True:
+                    location2 = input(
+                        "Enter your location (city): ").strip().title()
+                    if location2 != "":
+                        break
+                    else:
+                        print(
+                            "Dear user, Your city cannot be empty. Kindly input your city and try again")
+                    
+                while True:
+                    farmsize = input("kindly enter your farm size in acres: ")
+                    if farmsize != "":
+                        break
+                    else:
+                        print(
+                            "Dear user, Your farmsize cannot be empty. Kindly input your farmsize and try again")
+                while True:
+                    crops = input("kindly enter your primary crops: ")
+                    if farmsize != "":
+                        break
+                    else:
+                        print(
+                            "Dear user, Your primary cannot be empty. Kindly input your primary crops and try again")
+                farmer_dictionary = {
+                    "First Name": first_name,
+                    "Last Name": surname,
+                    "Phone Number": phonenumber,
+                    "State": location1,
+                    "City": location2,
+                    "Crops": crops,
+                    "Farm Size": farmsize
+
+                }
+                while True:
+
+                    confirm_details = int(input(
+                        f"Kindly Confirm your information\n{farmer_dictionary}\n1. Yes, Proceed \n2. No, Edit\nKindly choose: "))
+                    if confirm_details == 1:
+
+                        break
+                    elif confirm_details == 2:
+                        print("Try again !!!")
+                    else:
+                        print("Wrong input, Try again !!!")
+                while True:
+                    pin = (input("Enter Your 4 digit pin: "))
+                    if len(pin) == 4:
+                        break
+                    else:
+                        print("Ensure 4 Digit Pin is correct: ")
+                    
+        
+                reenter_pin = (input("Confirm your pin: "))
+                if reenter_pin == pin:
                     with open(farmers_csv, "a", newline="", encoding="utf-8") as f:
                         writer = csv.writer(f)
-                        writer.writerow([full_name, phone, state, city, farm_size, crops, pin])
+                        writer.writerow([surname, phonenumber, location1, location2, farmsize, crops, pin])
                     print("Farmer registered successfully!\n")
                 else:
-                    print("PINs do not match.\n")
+                    print("incorrect pin  verify your pin: ")
+                
             # Farmer Login
             elif menu_o == 2:
                 phone = input("Enter Phone Number: ")
@@ -60,24 +138,94 @@ while True:
                     print("Invalid login details for farmer.\n")
         # ---------------- Buyer Menu ----------------
         elif menu == 2:
-            menu_1 = int(input("1. Register as a buyer  2. Login as a buyer\nChoose Option: "))
-            # Buyer Registration
+            
+            menu_1 = int(
+                input("1. Register as a buyer  2. Login as a buyer\nChoose Option: "))
             if menu_1 == 1:
-                first_name = input("Enter Your First Name: ").strip().title()
-                last_name = input("Enter Your Last Name: ").strip().title()
-                full_name = f"{first_name} {last_name}"
-                phone = input("Enter Your Phone number (11 digits): ")
-                state = input("Enter your location (state): ").strip().title()
-                city = input("Enter your location (city): ").strip().title()
-                pin = input("Enter Your 4 digit pin: ")
-                re_pin = input("Confirm your pin: ")
-                if pin == re_pin:
-                    with open(buyers_csv, "a", newline="", encoding="utf-8") as f:
-                        writer = csv.writer(f)
-                        writer.writerow([full_name, phone, state, city, pin])
-                    print("white_tick: Buyer registered successfully!\n")
-                else:
-                    print("PINs do not match.\n")
+                while True:
+                    print("Enter your details")
+                    first_name = input(
+                        "Kindly enter your first name: ").strip().title()
+                    if first_name != "":
+                        break
+                    else:
+                        print(
+                            "Dear user, Your first name cannot be empty. Kindly input your first name and try again")
+
+                while True:
+                    last_name = input(
+                        "Kindly enter your last name: ").strip().title()
+                    if last_name != "":
+                        break
+                    else:
+                        print(
+                            "Dear user, Your last name cannot be empty. Kindly input your last name and try again")
+                full_name = (first_name, last_name)
+                while True:
+                    phone_number = input("Enter your phone number: ")
+                    if len(phone_number) == 11 and phone_number.isdigit():
+                        break
+                    else:
+                        print("Your phone number is invalid!!! Try again")
+
+                while True:
+                    location1 = input(
+                        "Enter your location (state): ").strip().title()
+                    if location1 != "":
+                        break
+                    else:
+                        print(
+                            "Dear user, Your state cannot be empty. Kindly input your state and try again")
+
+                while True:
+                    location2 = input(
+                        "Enter your location (city): ").strip().title()
+                    if location2 != "":
+                        break
+                    else:
+                        print(
+                            "Dear user, Your city cannot be empty. Kindly input your city and try again")
+                buyer_dictionary = {
+                    "First Name": first_name,
+                    "Last Name": last_name,
+                    "Phone Number": phone_number,
+                    "State": location1,
+                    "City": location2,
+                    
+                }
+                while True:
+
+                    confirm_details = int(input(
+                        f"Kindly Confirm your information\n{buyer_dictionary}\n1. Yes, Proceed \n2. No, Edit\nKindly choose: "))
+                    if confirm_details == 1:
+
+                        break
+                    elif confirm_details == 2:
+                        print("Try again !!!")
+                    else:
+                        print("Wrong input, Try again !!!")
+
+                while True:
+                    pin = input("Enter your 4 digit PIN: ")
+                    if len(pin) == 4 and pin.isdigit():
+                        break
+                    else:
+                        print("Your PIN is invalid!!! Try again")
+
+                while True:
+                    confirm_pin = input("Kindly confirm your PIN: ")
+                    if confirm_pin == pin:
+                        with open(buyers_csv, "a", newline="", encoding="utf-8") as f:
+                            writer = csv.writer(f)
+                            writer.writerow([full_name, phone_number, location1, location2, pin])
+                        print("Buyer registered successfully!\n")
+                    else:
+                        print(
+                            "You have entered a PIN different from the previous PIN. Please try again")
+
+                
+              
+              
             # Buyer Login
             elif menu_1 == 2:
                 phone = input("Enter Phone Number: ")
@@ -87,12 +235,13 @@ while True:
                     reader = csv.DictReader(f)
                     for row in reader:
                         if row["PHONE NUMBER"] == phone and row["PIN"] == pin:
-                            print(f":white_tick: Welcome back, {row['FULL NAME']}!")
+                            print(f"Welcome back, {row['FULL NAME']}!")
                             logged_in = row["FULL NAME"]
                             break
                 if logged_in == "":
                     print("Invalid login details for buyer.\n")
         else:
-            print("Invalid option, choose 1 or 2.\n")
+                print("Enter if you are a farmer or buyer")
+
     else:
-        print("Wrong code. Try again.\n")
+            print("Try again")
